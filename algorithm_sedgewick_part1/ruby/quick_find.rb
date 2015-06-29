6e4969b87a0d4aee095c50881527beeb9a0234cf
pavelperkin <perkinpv@gmail.com>
2015-06-29 21:26:53 +0300

    add tests and some bugfixing

--- algorithm_sedgewick_part1/ruby/quick_find.rb
+++ algorithm_sedgewick_part1/ruby/quick_find.rb
@@ -1,14 +1,40 @@
 class QuickFind
+  attr_accessor :list
 
   def initialize(n)
     @list = (0..n).to_a
   end
 
   def connected?(p, q)
-    @list[p] == @list[q]
+    list[p] == list[q]
   end
 
   def union p, q
-    @list.each {|el| @list[el] = p if @list[el] == q} 
+    pid, qid = [list[p], list[q]].sort
+    list.each {|el| list[el] = pid if list[el] == qid} 
+  end
+end
+
+
+require 'minitest/autorun'
+
+describe QuickFind do
+  it "should find and union nodes" do
+    qf = QuickFind.new(10)
+    qf.union(0,1)
+    qf.union(1,2)
+    qf.union(3,4)
+    qf.union(4,6)
+    qf.union(6,7)
+    qf.union(7,8)
+    qf.union(8,9)
+    qf.connected?(1,2).must_equal true
+    qf.connected?(0,2).must_equal true
+    qf.connected?(3,8).must_equal true
+    qf.connected?(5,5).must_equal true
+    qf.connected?(0,5).must_equal false
+    qf.connected?(4,5).must_equal false
+    qf.connected?(1,6).must_equal false
+    qf.connected?(8,5).must_equal false
   end
 end
